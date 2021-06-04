3afb8c7bbd70b674041780c633dcedbaf2f3b2bc
Uğur Üresin <uuresin@ford.com.tr>
2021-06-04 22:02:04 +0300

    add ch 03

---
+++ 03_CPP_FILE_IO.cpp
@@ -0,0 +1,51 @@
+
+/////////////////////////////////////////
+// WELCOME TO C++  PROGRAMMING! - 2019 //
+/////////////////////////////////////////
+
+/* This Code is Written By Ugur Uresin For training purposes! */
+
+/* 
+To compile the program: g++ filename.cpp -o executableName
+To execute the program: ./executableName
+*/
+
+// -------------------------------------------------- //
+// 03 - FILE IO
+// -------------------------------------------------- //
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+    string line;
+    //create an output stream to write to the file
+    //append the new lines to the end of the file
+    ofstream myfileI ("input.txt", ios::app);
+    if (myfileI.is_open())
+    {
+        myfileI << "\nI am adding a line.\n";
+        myfileI << "I am adding another line.\n";
+        myfileI.close();
+    }
+    else cout << "Unable to open file for writing";
+
+    //create an input stream to read the file
+    ifstream myfileO ("input.txt");
+    //During the creation of ifstream, the file is opened. 
+    //So we do not have explicitly open the file. 
+    if (myfileO.is_open())
+    {
+        while ( getline (myfileO,line) )
+        {
+            cout << line << '\n';
+        }
+        myfileO.close();
+    }
+
+    else cout << "Unable to open file for reading";
+
+    return 0;
+}
